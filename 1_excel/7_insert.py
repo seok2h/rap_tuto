f53ac22abf214bf22c48100f2730b15f79d5d769
Han Seungseok <cream5432111@gmail.com>
2024-02-29 22:44:24 +0900

    excel_7_insert

--- 1_excel/7_insert.py
+++ 1_excel/7_insert.py
@@ -2,6 +2,13 @@ from openpyxl import load_workbook
 wb = load_workbook("sample.xlsx")
 ws = wb.active
 
-ws.insert_rows(8)
+# ws.insert_rows(8)   # 8번째 줄이 비워지
+# ws.insert_rows(8, 5)    # 8번째 줄 위치에 5줄을 추가
 
+ws.insert_cols(2)
+ws.insert_cols(2, 3)
 wb.save("sample_insert_rows.xlsx")
+
+
+
+# wb.save("sample_insert_rows.xlsx")
