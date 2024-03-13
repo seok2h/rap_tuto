3de036746d7a3e43096096b08aa998e674b04f54
Han Seungseok <cream5432111@gmail.com>
2024-03-14 01:06:36 +0900

    9_message

---
+++ 2_desktop/9_message.py
@@ -0,0 +1,12 @@
+import pyautogui as pg
+
+# print("곧 시작합니다...")
+# pg.countdown(3)
+# print("자동화 시작")
+
+# result = pg.alert("자동화 수행에 실패하였습니다.", "경고")   # 확인 버튼만 있는 팝업
+# result = pg.confirm("계속 진행하시겠습니까?", "확인")    # 확인, 취소 버튼
+# result = pg.prompt("파일명을 무엇으로 하시겠습니까?", "입력")
+# result = pg.password("암호를 입력하세요") # 암호 입력
+
+# print(result)
