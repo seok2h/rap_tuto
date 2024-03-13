ef023ef17aa4782d29148feaa78b61595ab71977
Han Seungseok <cream5432111@gmail.com>
2024-03-14 00:24:37 +0900

    7_windows

---
+++ 2_desktop/7_windows.py
@@ -0,0 +1,26 @@
+import pyautogui as pg
+
+# fw = pg.getActiveWindow() # 현재 활성화 된 창
+# print(fw.title) # 창의 제목 정보
+# print(fw.size) # 창의 크기 정보
+# print(fw.left, fw.top, fw.right, fw.bottom) # 창의 좌표 정보
+# pg.click(fw.left + 10, fw.top + 20)
+
+# for w in pg.getAllWindows():
+#     print(w)
+
+# for w in pg.getWindowsWithTitle("visual"):
+#     print(w)
+# if w.isActive == False:   # 활성화가 되지 않았다면
+#     w.activate() # 활성화     # 맨 앞으로 가져오기
+
+# if w.isMaximized == False:   # 현재 최대화가 되지 않았다면
+#     w.maximize()    # 최대화
+
+# if w.isMinimized == False:  # 현재 최소화가 되지 않았다면
+#     w.minimize()    # 최소화
+
+# w.restore()     # 화면 원상복구
+
+# w.close()   # 창 닫기
+
